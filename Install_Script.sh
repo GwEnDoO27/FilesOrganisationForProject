2acba662272faf5cab854d327056aaa6b6e345d2
gbenard <gwendal.benard@gmail.com>
2024-07-26 14:04:37 +0200

    fix the script

--- Install_Script.sh
+++ Install_Script.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
-VERSION="v1.0.0"
-URL="https://github.com/votre-utilisateur/votre-repo/releases/download/$VERSION/monprogramme-linux"
+
+URL="https://raw.githubusercontent.com/GwEnDoO27/FilesOrganisationForProject/main/FilesOrganisationForProject-macos"
 
 # Téléchargement du binaire
-curl -L $URL -o /usr/local/bin/monprogramme
+echo "Téléchargement du binaire depuis $URL"
+curl -L $URL -o /usr/local/bin/FilesOrganisationForProject
 
-# Rendre le binaire exécutable
-chmod +x /usr/local/bin/monprogramme
-
-# Ajouter un alias dans le fichier .bashrc ou .zshrc
-if [ -n "$ZSH_VERSION" ]; then
-    SHELL_PROFILE="$HOME/.zshrc"
-else
-    SHELL_PROFILE="$HOME/.bashrc"
+# Vérification si le téléchargement a réussi
+if [ $? -ne 0 ]; then
+    echo "Échec du téléchargement du binaire."
+    exit 1
 fi
 
-echo "alias monprog='/usr/local/bin/monprogramme'" >> $SHELL_PROFILE
+# Rendre le binaire exécutable
+sudo chmod +x /usr/local/bin/FilesOrganisationForProject
+
+# Vérifier le shell et définir le fichier de profil approprié
+if [ "$SHELL" = "/bin/zsh" ] || [ "$SHELL" = "/usr/bin/zsh" ]; then
+    SHELL_PROFILE="$HOME/.zshrc"
+elif [ "$SHELL" = "/bin/bash" ] || [ "$SHELL" = "/usr/bin/bash" ]; then
+    SHELL_PROFILE="$HOME/.bashrc"
+else
+    SHELL_PROFILE="$HOME/.profile"
+fi
+# Ajouter un alias dans le fichier de profil
+if grep -Fxq "alias Newproject='/usr/local/bin/FilesOrganisationForProject'" $SHELL_PROFILE; then
+    echo "Alias 'Newproject' déjà présent dans $SHELL_PROFILE"
+else
+    echo "alias Newproject='/usr/local/bin/FilesOrganisationForProject'" >> $SHELL_PROFILE
+    echo "Alias 'Newproject' ajouté dans $SHELL_PROFILE"
+fi
+
+# Recharger le fichier de profil
 source $SHELL_PROFILE
 
-echo "Installation terminée. Vous pouvez utiliser le programme en tapant 'monprog'."
+echo "Installation terminée. Vous pouvez utiliser le programme en tapant 'Newproject'."
+
